8857afca134c5695870458af7d185172712fdae5
benqy <hmjlr123@gmail.com>
2023-07-13 15:17:36 +0800

    vue

---
+++ packages/client/src/main.ts
@@ -0,0 +1,9 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import * as benqy from '@benqy/common'
+
+console.log(benqy)
+
+createApp(App).mount('#app')
---
+++ packages/common/index.ts
@@ -0,0 +1,1 @@
+export const common = 'commo1hhhhhhhhn'
