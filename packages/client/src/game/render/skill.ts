c6895e39388f5f52c3f173ca95213b894edd6269
benqy <hmjlr123@gmail.com>
2023-07-30 01:30:29 +0800

    蓝火球

--- packages/client/src/game/render/skill.ts
+++ packages/client/src/game/render/skill.ts
@@ -19,6 +19,7 @@ import {
 } from 'pixi.js'
 import { game } from '../game'
 import { fire16 } from './sheet/fire16'
+import { boltBlue } from './sheet/bolt-blue'
 
 export class SkillRender {
   skillSheets: Map<string, Spritesheet> = new Map()
@@ -27,7 +28,7 @@ export class SkillRender {
     if (!game.character || !game.scene.mainEnemy.value) return
     let spritesheet: Spritesheet
     if (!this.skillSheets.has(skill.name.toLocaleLowerCase())) {
-      spritesheet = new Spritesheet(BaseTexture.from(fire16.meta.image), fire16)
+      spritesheet = new Spritesheet(BaseTexture.from(boltBlue.meta.image), boltBlue)
       this.skillSheets.set(skill.name.toLocaleLowerCase(), spritesheet)
       await spritesheet.parse()
     } else {
@@ -36,12 +37,12 @@ export class SkillRender {
     const anim = new AnimatedSprite(spritesheet.animations.fireball)
     anim.x = game.character.renderData.x
     anim.y = game.character.renderData.y
-    anim.angle = -90
+    anim.angle = 90
     anim.anchor.set(0.5)
     anim.animationSpeed = 0.5
     anim.play()
     this.app.stage.addChild(anim)
-    const speed = 15
+    const speed = 10
     const enemyRenderData = game.scene.mainEnemy.value.renderData
     const listener = (deltaTime: number) => {
       anim.y -= speed * deltaTime
